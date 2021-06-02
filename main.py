1e634cd6d5e718bdc6505e6c821d897879739a3b
magikarp01 <53954951+magikarp01@users.noreply.github.com>
2021-06-02 18:50:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ testPositions3 = [[1,0,0,0,0,0],[1,0,0,0,0,0],[2,0,0,0,0,0],[2,2,1,0,0,0],[2,0,0
 testPositions4 = [[1,2,2,2,1,0],[2,1,2,1,0,0],[1,0,0,0,0,0],[1,1,2,1,2,0],[0]*6,[2,2,2,1,0,0],[1,0,0,0,0,0]]
 
 # the testPositions can be any of the 4
-comparison.OCBAGame(1, [5, 50], 3)
+comparison.comparePlot(1, [10, 300], 10, 2, testPositions3)
+# comparison.OCBAGame(1, [5, 50], 3)
 # comparison.UCBGame(1, [5, 50], 2)
-# comparison.comparePlot(1, [10, 300], 10, 2, testPositions3)
-# comparison.compareDepth(3, 20, 2, 2, testPositions2)
 # comparison.playAgainst(1, [10, 50], 3, 2, 1)
+# comparison.compareDepth(3, 20, 2, 2, testPositions2)
