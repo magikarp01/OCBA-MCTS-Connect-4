c6f3c811e367b5c276117477893b21cf1b2783a3
Phillip Guo <philliphguo@gmail.com>
2021-11-23 10:55:55 -0500

    Stable

---
+++ main.py
@@ -0,0 +1,22 @@
+from OCBA import OCBAState
+from UCT import UCBState
+import comparison
+
+def convertPrintpos():
+    rows = []
+    for i in range(6):
+        row = input()
+        row=row.split()
+
+# testPositions1 = [[0]*6, [0]*6, [0]*6, [0]*6, [0]*6, [0]*6, [0]*6]
+# testPositions2 = [[0]*6, [0]*6, [1,1,2,1,0,0], [2,2,1,0,0,0], [2,2,0,0,0,0], [1,1,0,0,0,0], [0]*6]
+# testPositions3 = [[1,0,0,0,0,0],[1,0,0,0,0,0],[2,0,0,0,0,0],[2,2,1,0,0,0],[2,0,0,0,0,0],[1,0,0,0,0,0],[1,0,0,0,0,0]]
+# testPositions4 = [[1,2,2,2,1,0],[2,1,2,1,0,0],[1,0,0,0,0,0],[1,1,2,1,2,0],[0]*6,[2,2,2,1,0,0],[1,0,0,0,0,0]]
+jonahGamePos = [[0,0,0,0,0,0],[0,0,0,0,0,0],[2,0,0,0,0,0],[1,1,2,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+
+# the testPositions can be any of the 4
+# comparison.comparePlot(1, [10, 300], 10, 2, testPositions3)
+comparison.OCBAGame(1, [5, 50], 3, positions=jonahGamePos)
+# comparison.UCBGame(1, [5, 50], 2)
+# comparison.playAgainst(1, [10, 50], 3, 2, 1)
+# comparison.compareDepth(3, 20, 2, 2, testPositions2)
